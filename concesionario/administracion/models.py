68483e29df7f4d03e5b7d3ff910ed5366faf0d2c
danielflopez <daniel.f.lopez.j@gmail.com>
2015-12-19 12:24:57 -0500

    Agregue la llave primaria y cambie la llave foranea de empleado

--- concesionario/administracion/models.py
+++ concesionario/administracion/models.py
@@ -6,7 +6,8 @@ from django.contrib.auth.models import User
 
 class empleado(models.Model):
 
-    id_empleado = models.ForeignKey(User, unique=True)
+    user = models.ForeignKey(User, unique=True)
+    id_empleado = models.AutoField(primary_key=True)
     cedula = models.CharField(max_length=10, blank=True)
     fecha_de_nacimiento = models.DateTimeField(max_lengt=8, blank=True)
     direccion = models.CharField(max_length=100, blank=True)
