f3ab9244881e4dc3632da10119dcf30eed0b0ec6
andresfaq <andres.artunduaga@softpowering.com>
2015-12-25 15:26:41 -0500

    descargando

--- concesionario/administracion/models.py
+++ concesionario/administracion/models.py
@@ -9,7 +9,7 @@ class empleado(models.Model):
     user = models.ForeignKey(User, unique=True)
     id_empleado = models.AutoField(primary_key=True)
     cedula = models.CharField(max_length=10, blank=True)
-    fecha_de_nacimiento = models.DateTimeField(max_lengt=8, blank=True)
+    fecha_de_nacimiento = models.DateTimeField(max_length=8, blank=True)
     direccion = models.CharField(max_length=100, blank=True)
     telefono = models.PositiveIntegerField(null=True, blank=True)
     inicio_contrato = models.DateTimeField(max_length=8, blank=True)
