07ea2207010dc876cbc32c76b4f6518d0fe60c22
usuarioVM <usuario@usuario.com>
2016-01-12 16:25:19 -0500

    agregadas paginas de 4 reportes

--- concesionario/reportes/urls.py
+++ concesionario/reportes/urls.py
@@ -3,6 +3,8 @@ from reportes import views
 
 urlpatterns = [
 				url(r'^$', views.inicio),
-				url(r'^usuarios$', views.usuarios)]
+				url(r'^usuarios$', views.usuarios),
+				url(r'^inventario$', views.inventario),
+				url(r'^ventas$', views.ventas)]
 
 
