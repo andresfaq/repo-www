b54f0358bd72a9dfc27ed17257d3f2bc55c076ce
usuarioVM <usuario@usuario.com>
2016-01-13 17:33:17 -0500

    reportes de ventas y usuarios

--- concesionario/reportes/urls.py
+++ concesionario/reportes/urls.py
@@ -1,10 +1,17 @@
 from django.conf.urls import patterns, url, include
 from reportes import views
 
+from tastypie.api import Api
+from reportes.api import UserResource
+
+v1_api = Api(api_name='v1')
+v1_api.register(UserResource())
+
 urlpatterns = [
 				url(r'^$', views.inicio),
 				url(r'^usuarios$', views.usuarios),
 				url(r'^inventario$', views.inventario),
-				url(r'^ventas$', views.ventas)]
+				url(r'^ventas$', views.ventas),
+				url(r'^api/', include(v1_api.urls))]
 
 
--- concesionario/reportes/views.py
+++ concesionario/reportes/views.py
@@ -1,13 +1,15 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from administracion.models import User, Empleado
 
 @login_required
 def inicio(request):
-    return render(request, 'reportes/index.html')
+	return render(request, 'reportes/index.html')
 
 @login_required
 def usuarios(request):
-    return render(request, 'reportes/usuarios.html')
+	usuarios = User.objects.all()
+	return render(request, 'reportes/usuarios.html',{'usuarios':usuarios})
 
 @login_required
 def inventario(request):
@@ -15,6 +17,11 @@ def inventario(request):
 
 @login_required
 def ventas(request):
-    return render(request, 'reportes/ventas.html')
+	usuarios = User.objects.all()
+	return render(request, 'reportes/ventas.html',{'usuarios':usuarios})
 
+# u1= User.objects.create(id='1', password='123456', last_login='2016-01-13', is_superuser='True', username='daniel', first_name='daniel', last_name='lopez', email='daniel@daniel.com',
+#  is_staff='True', is_active='True', date_joined='2016-01-01', cedula='123456789', direccion='cra 123 # 123', fecha_de_nacimiento='1990-01-01', telefono='1234567')
+
+# e1 = Empleado.objects.create(id_usuario='1',id_empleado="1",incio_contrato='2000-01-01', fin_contrato='2111-01-01',salario='9999999')
 
