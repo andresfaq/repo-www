fb3a3333c64d1c289d2bf9664263f5617344e933
andresfaq <andres.artunduaga@softpowering.com>
2016-01-15 03:55:21 -0500

    generador datos

--- concesionario/administracion/seeders.py
+++ concesionario/administracion/seeders.py
@@ -1,48 +1,118 @@
+from administracion.factories import SuperUserFactory, GerenteFactory, VendedorFactory, JefeTallerFactory, ClienteFactory, OrdenFactory, RepuestoFactory, RepuestosPorOrdenFactory, InventarioRepuestoFactory, VehiculoFactory, RevisionVehiculoFactory, InventarioVehiculoFactory
+from administracion.models import User, Sucursal, Gerente, Vendedor, JefeTaller, Cliente, Orden, Repuesto, RepuestosPorOrden, InventarioRepuesto, Vehiculo, RevisionVehiculo, InventarioVehiculo
 
-# import autofixture
-# from django.contrib.auth.models import User
-# from administracion.models import *
-#
-# suc = Sucursal.objects.all()
-# for x in suc:
-#     x.delete()
-#
-# ven = Vendedor.objects.all()
-# for x in ven:
-#     x.delete()
-#
-# ger = Gerente.objects.all()
-# for x in ger:
-#     x.delete()
-#
-# emp = Empleado.objects.all()
-# for x in emp:
-#     x.delete()
-#
-# usr = User.objects.all()
-# for x in usr:
-#     x.delete()
-#
-# usuarios = autofixture.create('auth.User', 10)
-# for u in usuarios:
-#     User.objects._insert(u)
-#
-# empleados = autofixture.create('administracion.Empleado', 10, follow_fk=True)
-# for e in empleados:
-#     Empleado.objects._insert(e)
-#
-# sucursales= autofixture.create('administracion.Sucursal', 2)
-# for s in sucursales:
-#     Sucursal.objects._insert(s)
-#
-# gerentes = autofixture.create('administracion.Gerente', 2, follow_fk=True)
-# for g in gerentes:
-#     Gerente.objects._insert(g)
-#
-# vendedores = autofixture.create('administracion.Vendedor', 6, follow_fk=True)
-# for v in vendedores:
-#     Vendedor.objects._insert(v)
-#
-# jefestaller = autofixture.create('administracion.JefeTaller', 2, follow_fk=True)
-# for jt in jefestaller:
-#     JefeTaller.objects._insert(jt)
+#Eliminando RevisionVehiculo
+reveh = RevisionVehiculo.objects.all()
+for x in reveh:
+    x.delete()
+
+#Eliminando Vehiculo
+veh = Vehiculo.objects.all()
+for x in veh:
+    x.delete()
+
+#Eliminando InventarioRepuestos
+invrep = InventarioRepuesto.objects.all()
+for x in invrep:
+    x.delete()
+
+#Eliminando repuestos por orden
+repord = RepuestosPorOrden.objects.all()
+for x in repord:
+    x.delete()
+
+#Eliminando Repuestos
+rep = Repuesto.objects.all()
+for x in rep:
+    x.delete()
+
+#Eliminando Ordenes
+ord = Orden.objects.all()
+for x in ord:
+    x.delete()
+
+#Eliminando Sucursales (las sucursales se crean al crear los gerentes)
+suc = Sucursal.objects.all()
+for x in suc:
+    x.delete()
+
+#Eliminando Clientes
+usr = Cliente.objects.all()
+for x in usr:
+    x.delete()
+
+#Eliminando Gerentes
+usr = Gerente.objects.all()
+for x in usr:
+    x.delete()
+
+#Eliminando Vendedores
+usr = Vendedor.objects.all()
+for x in usr:
+    x.delete()
+
+#Eliminando JefesTaller
+usr = JefeTaller.objects.all()
+for x in usr:
+    x.delete()
+
+#Eliminando Usuarios
+usr = User.objects.all()
+for x in usr:
+    x.delete()
+
+#===================================================================================
+
+
+#Creando superusuario
+User.save(SuperUserFactory.create())
+
+#Creando Clientes
+for x in range(250):
+    Gerente.save(ClienteFactory.create())
+
+#Creando Gerentes
+for x in range(3):
+    Gerente.save(GerenteFactory.create())
+
+#Creando Vendedores
+for x in range(18):
+    Vendedor.save(VendedorFactory.create())
+
+#Creando JefesTaller
+for x in range(6):
+    JefeTaller.save(JefeTallerFactory.create())
+
+#Creando Ordenes
+for x in range(60):
+    Orden.save(OrdenFactory.create())
+
+
+#Creando Vehiculos
+for x in range(20):
+    Vehiculo.save(VehiculoFactory.create())
+
+
+#Creando RevisionVehiculo = #Ordenes
+for x in range(60):
+    RevisionVehiculo.save(RevisionVehiculoFactory.create())
+
+#Creando InventarioVehiculo = #Vehiculo
+for x in range(20):
+    InventarioVehiculo.save(InventarioVehiculoFactory.create())
+
+
+
+#Creando Repuestos
+for x in range(120):
+    Repuesto.save(RepuestoFactory.create())
+
+#Creando InventarioRepuesto << Se crea la misma cantidad que repuestos o menor
+for x in range(120):
+    InventarioRepuesto.save(InventarioRepuestoFactory.create())
+
+#Creando RepuestosPorOrden
+for x in range(150):
+    Repuesto.save(RepuestosPorOrdenFactory.create())
+
+
