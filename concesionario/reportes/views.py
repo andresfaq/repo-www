cdff390743086932223e7336960786c78822997b
danielflopez <daniel.f.lopez.j@gmail.com>
2016-01-20 22:01:12 -0500

    reporte de ventas y usuarios funcional

--- concesionario/reportes/views.py
+++ concesionario/reportes/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from administracion.models import User, Empleado
+from administracion.models import User, Empleado, Vendedor, Venta, Vehiculo
 
 @login_required
 def inicio(request):
@@ -8,20 +8,25 @@ def inicio(request):
 
 @login_required
 def usuarios(request):
+
 	usuarios = User.objects.all()
-	return render(request, 'reportes/usuarios.html',{'usuarios':usuarios})
+
+	return render(request, 'reportes/usuarios.html', {'usuarios':usuarios})
 
 @login_required
 def inventario(request):
-    return render(request, 'reportes/inventario.html')
+	return render(request, 'reportes/inventario.html')
 
 @login_required
 def ventas(request):
-	usuarios = User.objects.all()
-	return render(request, 'reportes/ventas.html',{'usuarios':usuarios})
 
-# u1= User.objects.create(id='1', password='123456', last_login='2016-01-13', is_superuser='True', username='daniel', first_name='daniel', last_name='lopez', email='daniel@daniel.com',
-#  is_staff='True', is_active='True', date_joined='2016-01-01', cedula='123456789', direccion='cra 123 # 123', fecha_de_nacimiento='1990-01-01', telefono='1234567')
+	vendedores = Vendedor.objects.all()
+	ventas = Venta.objects.all()
 
-# e1 = Empleado.objects.create(id_usuario='1',id_empleado="1",incio_contrato='2000-01-01', fin_contrato='2111-01-01',salario='9999999')
+	for vendedor in vendedores:
 
+		#vendedor.num_ventas = len(vendedor.filter(codigo_vendedor=ventas.codigo_vendedor)
+		vendedor.num_ventas = len(ventas.filter(codigo_vendedor=vendedor.codigo_vendedor))
+		print(vendedor.first_name, vendedor.username, vendedor.num_ventas, vendedor.id_empleado, vendedor.codigo_vendedor)
+
+	return render(request, 'reportes/ventas.html', {'vendedores':vendedores})
