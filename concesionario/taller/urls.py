64a664e5ae982ee1d1d4a7ac51e05d6ac5959e36
danielflopez <daniel.f.lopez.j@gmail.com>
2016-01-26 14:40:20 -0500

    no se 2

---
+++ concesionario/taller/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, url, include
+from taller import views
+
+
+urlpatterns = [url(r'^$', views.inicio, name='inicio_taller'),
+               url(r'^ingresarVehiculo/$', views.ingresarVehiculo, name='ingresarVehiculo'),]
+               #url(r'^busqueda/$', views.busquedaCodigoVenta, name='busquedaCodigoVenta'), ]
+
+# urlpatterns = patterns('concesionario.taller.views',
+#                        url(r'^$', views.inicio),
+#                        url(r'^ingresarVehiculo/$', views.ingresarVehiculo, name='ingresarVehiculo')
+#                        )
