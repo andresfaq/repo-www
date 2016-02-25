96eedda6826d2c329a2a48548bc6161247eb33bc
danielflopez <daniel.f.lopez.j@gmail.com>
2016-02-25 12:58:38 -0500

    heroku

--- concesionario/concesionario/settings/base.py
+++ concesionario/concesionario/settings/base.py
@@ -102,16 +102,16 @@ WSGI_APPLICATION = 'concesionario.wsgi.application'
 #     }
 # }
 
- DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-         'NAME': 'dca1petso80la5',
-         'USER': 'nrajgywlyspilh',
-         'PASSWORD': 'lKZRK6HIj0GnEVcWwvg9KMefjc',
-         'HOST': 'ec2-54-243-132-114.compute-1.amazonaws.com',
-         'PORT': '5432',
-     }
- }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dca1petso80la5',
+       'USER': 'nrajgywlyspilh',
+        'PASSWORD': 'lKZRK6HIj0GnEVcWwvg9KMefjc',
+        'HOST': 'ec2-54-243-132-114.compute-1.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 
 
 
