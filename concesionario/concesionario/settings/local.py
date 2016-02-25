73c5443703896189656222f9ce6d25365597ef2a
andresfaq <andres.artunduaga@softpowering.com>
2016-02-25 00:52:32 -0500

    actualizando datos bd heroku

--- concesionario/concesionario/settings/local.py
+++ concesionario/concesionario/settings/local.py
@@ -92,14 +92,13 @@ WSGI_APPLICATION = 'concesionario.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'concesionario',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': '127.0.0.1',
+        'NAME': 'dca1petso80la5',
+        'USER': 'nrajgywlyspilh',
+        'PASSWORD': 'lKZRK6HIj0GnEVcWwvg9KMefjc',
+        'HOST': 'ec2-54-243-132-114.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
