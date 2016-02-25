e1a8c0173379f14642736b8ae8a1ac353ab9dc2e
andresfaq <andres.artunduaga@softpowering.com>
2016-02-25 17:35:04 -0500

    gestionando migraciones heroku

--- concesionario/concesionario/wsgi.py
+++ concesionario/concesionario/wsgi.py
@@ -10,6 +10,9 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prompt.settings")
+
 from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "concesionario.settings")
